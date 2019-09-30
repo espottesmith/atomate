16e9ff1e2113f47c362165285dfc6251d4267884
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2019-09-30 09:58:45 -0700

    Started on test for ts_search with FSM (still need a successful test for GSM); tests are not passing yet; to make them pass will require changes to RunQChemFake

--- atomate/qchem/workflows/tests/test_ts_search.py
+++ atomate/qchem/workflows/tests/test_ts_search.py
@@ -11,7 +11,7 @@ from atomate.utils.testing import AtomateTest
 from pymatgen.core import Molecule
 from pymatgen.io.qchem.inputs import QCInput
 from atomate.qchem.powerups import use_fake_qchem
-from atomate.qchem.workflows.base.test_ts import get_wf_ts_search
+from atomate.qchem.workflows.base.ts_search import get_wf_ts_search
 
 __author__ = "Evan Spotte-Smith"
 __copyright__ = "Copyright 2019, The Materials Project"
@@ -28,8 +28,61 @@ db_dir = os.path.join(module_dir, "..", "..", "..", "common", "test_files")
 
 
 class TestTSSearch(AtomateTest):
-    def test_ts_search(self):
-        pass
+    def test_ts_search_fsm(self):
+        # location of test files
+        ts_search_files = os.path.join(module_dir, "..", "..",
+                                       "test_files", "fsm_ts_freq")
+        # define starting molecule and workflow object
+        initial_qcin = QCInput.from_file(
+            os.path.join(ts_search_files, "block", "launcher_first",
+                         "mol.qin"))
+        initial_mol = initial_qcin.molecule
+
+        params = {"dft_rung": 3,
+                  "basis_set": "6-31g*",
+                  "overwrite_inputs": {"rem": {"xc_grid": 3}}}
+
+        real_wf = get_wf_ts_search(
+            reactants=initial_mol["reactants"],
+            products=initial_mol["products"],
+            qchem_input_params=params,
+            linked=True,
+            multimode="openmp",
+            method="fsm")
+        # use powerup to replace run with fake run
+        ref_dirs = {
+            "ts_search_fsm":
+            os.path.join(ts_search_files, "block", "launcher_first"),
+            "ts_search_ff_ts":
+            os.path.join(ts_search_files, "block", "launcher_second")
+        }
+        fake_wf = use_fake_qchem(real_wf, ref_dirs)
+        self.lp.add_wf(fake_wf)
+        rapidfire(
+            self.lp,
+            fworker=FWorker(env={"max_cores": 32, "db_file": os.path.join(db_dir, "db.json"),
+                                 "qchem_cmd": ""}))
+
+        wf_test = self.lp.get_wf_by_fw_id(1)
+        self.assertTrue(
+            all([s == "COMPLETED" for s in wf_test.fw_states.values()]))
+
+        fsm = self.get_task_collection().find_one({
+            "task_label":
+            "ts_search_fsm"
+        })
+
+        fsm_ts_guess_mol = Molecule.from_dict(fsm["output"]["ts_guess"])
+
+        ff_ts = self.get_task_collection().find_one({
+            "task_label":
+            "ts_search_ff_ts"
+        })
+
+        ff_ts_initial_mol = Molecule.from_dict(
+            ff_ts["input"]["initial_molecule"])
+
+        self.assertEqual(fsm_ts_guess_mol, ff_ts_initial_mol)
 
 
 if __name__ == "__main__":
