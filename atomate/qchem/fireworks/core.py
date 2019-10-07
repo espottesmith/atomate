c9aae6d5818cdafb3c5fa998311b0842111a179d
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2019-10-07 10:23:48 -0700

    Added FW for PES_SCAN (still needs to be tested)

--- atomate/qchem/fireworks/core.py
+++ atomate/qchem/fireworks/core.py
@@ -559,6 +559,86 @@ class GrowingStringFW(Firework):
             **kwargs)
 
 
+class PESScanFW(Firework):
+    def __init__(self,
+                 molecule=None,
+                 name="potential energy surface scan",
+                 qchem_cmd=">>qchem_cmd<<",
+                 multimode=">>multimode<<",
+                 max_cores=">>max_cores<<",
+                 qchem_input_params=None,
+                 scan_variables=None,
+                 db_file=None,
+                 parents=None,
+                 **kwargs):
+        """
+        Perform a potential energy surface scan by varying bond lengths, angles,
+        and/or dihedral angles in a molecule.
+
+        Args:
+           molecule (Molecule): Input molecule.
+            name (str): Name for the Firework.
+            qchem_cmd (str): Command to run QChem. Supports env_chk.
+            multimode (str): Parallelization scheme, either openmp or mpi. Supports env_chk.
+            max_cores (int): Maximum number of cores to parallelize over. Supports env_chk.
+            qchem_input_params (dict): Specify kwargs for instantiating the input set parameters.
+                                       Basic uses would be to modify the default inputs of the set,
+                                       such as dft_rung, basis_set, pcm_dielectric, scf_algorithm,
+                                       or max_scf_cycles. See pymatgen/io/qchem/sets.py for default
+                                       values of all input parameters. For instance, if a user wanted
+                                       to use a more advanced DFT functional, include a pcm with a
+                                       dielectric of 30, and use a larger basis, the user would set
+                                       qchem_input_params = {"dft_rung": 5, "pcm_dielectric": 30,
+                                       "basis_set": "6-311++g**"}. However, more advanced customization
+                                       of the input is also possible through the overwrite_inputs key
+                                       which allows the user to directly modify the rem, pcm, smd, and
+                                       solvent dictionaries that QChemDictSet passes to inputs.py to
+                                       print an actual input file.
+            scan_variables (dict): dict {str: list}, where the key is the type of variable ("stre"
+                                   for bond length, "bend" for angle, "tors" for dihedral angle),
+                                   and the list contains all of the variable set information
+            db_file (str): Path to file specifying db credentials to place output parsing.
+            parents ([Firework]): Parents of this particular Firework.
+            **kwargs: Other kwargs that are passed to Firework.__init__.
+        """
+
+        if scan_variables is None:
+            raise ValueError("Some variable input must be given! Provide some "
+                             "bond, angle, or dihedral angle information.")
+
+        qchem_input_params = qchem_input_params or dict()
+        qchem_input_params["scan_variables"] = scan_variables
+        input_file = "mol.qin"
+        output_file = "mol.qout"
+        t = list()
+
+        t.append(
+            WriteInputFromIOSet(
+                molecule=molecule,
+                qchem_input_set="PESScanSet",
+                input_file=input_file,
+                qchem_input_params=qchem_input_params))
+        t.append(
+            RunQChemCustodian(
+                qchem_cmd=qchem_cmd,
+                multimode=multimode,
+                input_file=input_file,
+                output_file=output_file,
+                max_cores=max_cores,
+                job_type="normal"))
+        t.append(
+            QChemToDb(
+                db_file=db_file,
+                input_file=input_file,
+                output_file=output_file,
+                additional_fields={"task_label": name}))
+        super(PESScanFW, self).__init__(
+            t,
+            parents=parents,
+            name=name,
+            **kwargs)
+
+
 class FrequencyFlatteningOptimizeFW(Firework):
     def __init__(self,
                  molecule=None,
