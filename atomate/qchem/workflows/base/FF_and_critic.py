d5cd9f316bc981650ddebe8d5745e6d6664c741e
Evan Walter Clark Spotte-Smith <espottesmith@gmail.com>
2020-03-28 11:01:06 -0700

    Added WF for FFTSOpt -> Critic

--- atomate/qchem/workflows/base/FF_and_critic.py
+++ atomate/qchem/workflows/base/FF_and_critic.py
@@ -25,7 +25,7 @@ def get_wf_FFopt_and_critic(molecule,
          max_cores=">>max_cores<<",
          qchem_input_params=qchem_input_params,
          linked=True,
-         db_file=">>db_file<<"
+         db_file=db_file
     )
 
     # Critic
@@ -34,7 +34,41 @@ def get_wf_FFopt_and_critic(molecule,
          qchem_cmd=">>qchem_cmd<<",
          max_cores=">>max_cores<<",
          qchem_input_params=qchem_input_params,
-         db_file=">>db_file<<",
+         db_file=db_file,
+         parents=fw1)
+    fws = [fw1, fw2]
+
+    wfname = "{}:{}".format(molecule.composition.alphabetical_formula, "FFopt_CC2_WF_" + suffix)
+
+    return Workflow(fws, name=wfname, **kwargs)
+
+
+def get_wf_FFTSopt_and_critic(molecule,
+                            suffix,
+                            qchem_input_params=None,
+                            db_file=">>db_file<<",
+                            **kwargs):
+    """
+    """
+
+    # FFopt
+    fw1 = FrequencyFlatteningOptimizeFW(
+         molecule=molecule,
+         name="{}:{}".format(molecule.composition.alphabetical_formula, "FFopt_" + suffix),
+         qchem_cmd=">>qchem_cmd<<",
+         max_cores=">>max_cores<<",
+         qchem_input_params=qchem_input_params,
+         linked=True,
+         db_file=db_file
+    )
+
+    # Critic
+    fw2 = CubeAndCritic2FW(
+         name="{}:{}".format(molecule.composition.alphabetical_formula, "CC2_" + suffix),
+         qchem_cmd=">>qchem_cmd<<",
+         max_cores=">>max_cores<<",
+         qchem_input_params=qchem_input_params,
+         db_file=db_file,
          parents=fw1)
     fws = [fw1, fw2]
 
